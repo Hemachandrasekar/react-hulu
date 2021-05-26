6fec0eab7d564be99c3656dce6cdd1172582d062
Hema Victor <m.hema10@gmail.com>
2021-05-26 17:00:34 +0100

    loader and lazyloader added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, useCallback } from "react";
 import Header from "./component/Header";
 import "./App.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Navbar from "./component/Navbar";
 import MovieList from "./component/MoviesList";
-import { useLocation } from "react-router-dom";
+import Loader from "react-loader-spinner";
+import { useLocation, useHistory } from "react-router-dom";
 
 const App = () => {
   // eslint-disable-next-line
   let location = useLocation();
   const [list, setList] = useState([]);
+  const [loader, setLoader] = useState(true);
+  let history = useHistory();
 
-  const fetchMovies = (location) => {
-    console.log("location value", location);
-
-    if (location === null) {
-    } else {
-      location = "/trending";
-    }
+  const fetchMovies = useCallback((location) => {
     fetch(
       `https://api.themoviedb.org/3${location}/all/week?api_key=f64c70cd7a57c5893c2c78f4f6bc9165&language=en-us`
     )
       .then((res) => res.json())
       .then((data) => {
+        setLoader(false);
         setList(data.results);
+      })
+      .catch(() => {
+        setLoader(false);
       });
-  };
+  }, []);
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
-  useEffect(() => {
+    location.pathname === "/" && history.push("/trending");
     fetchMovies(location.pathname);
-  }, [location]);
+  }, [location, fetchMovies, history]);
 
   return (
     <div className="bg-body text-gray-100 h-full ">
+      {loader && (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={50}
+          width={50}
+          className="loader"
+        />
+      )}
+
       <Header />
       <Navbar />
       <MovieList list={list} />
--- src/component/MoviesList/index.jsx
+++ src/component/MoviesList/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const MovieList = ({ list }) => {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
 
   return (
-    <div className="px-5 my-2 sm:grid md:grid-cols-3 cursor-pointer ">
+    <div className="px-5 my-4 sm:grid md:grid-cols-3 cursor-pointer ">
       {list?.map((item, index) => {
         return (
           <div className="flex flex-col p-2 transition duration-200 ease-in transform sm:hover:scale-105 group hover:z-50">
-            <img src={`${BASE_URL}${item.backdrop_path}`} alt={item.title} />
+            <LazyLoadImage
+              alt={item.title}
+              effect="blur"
+              src={`${BASE_URL}${item.backdrop_path}`}
+            />
             <div className="p-2">
               <p className="truncate max-w-md">{item.overview}</p>
               <h2 className="mt-2 text-white text-2xl transition-all duration-100 ease-in-out ">
